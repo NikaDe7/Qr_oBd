d95a83b6e4785b2c495c3e41cdb17472727fd041
Tanya <vorobest5@gmail.com>
2024-03-23 21:10:41 +0200

    add table for 7 columns

--- db.py
+++ db.py
@@ -3,55 +3,68 @@ import sqlite3
 connect = sqlite3.connect('baza/list.db')
 cursor = connect.cursor()
 
-#Створити aдаптована таблиця з 1 колонкою
+#Створити aдаптовану таблицю з 1 колонкою
 def db_plus1(name_table ,name1, type1):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""(""" + name1 + type1 + """) """)
     connect.commit()
 
-#Створити aдаптована таблиця з 2 колонками
+#Створити aдаптовану таблицю з 2 колонкою
 def db_plus2(name_table, name1, type1, name2, type2):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""(""" + name1 + type1 + """, """ + name2 + type2 + """) """)
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """) """)
     connect.commit()
 
-#Створити базу даних для 3 колонок
+#Створити адаптовану таблицю для 3 колонок
 def db_plus3(name_table, name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""(name TEXT, last_name TEXT, id INT) """)
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """, """
+                   + name3 + type3 + """) """)
     connect.commit()
 
-#Створити базу даних для 4 колонок
-def db_plus4(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
+#Створити адаптовану таблицю для 4 колонок
+def db_plus4(name_table, name1, type1, name2, type2, name3, type3, name4, type4):
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """, """
+                   + name3 + type3 + """, """
+                   + name4 + type4 + """) """)
     connect.commit()
 
-#Створити базу даних для 5 колонок
-def db_plus5(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
+#Створити адаптовану таблицю для 5 колонок
+def db_plus5(name_table, name1, type1, name2, type2, name3, type3, name4, type4, name5, type5):
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """, """
+                   + name3 + type3 + """, """
+                   + name4 + type4 + """, """
+                   + name5 + type5 + """) """)
     connect.commit()
 
-#Створити базу даних для 6 колонок
-def db_plus6(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
+#Створити адаптовану таблицю для 6 колонок
+def db_plus6(name_table, name1, type1, name2, type2, name3, type3, name4, type4, name5, type5, name6, type6):
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """, """
+                   + name3 + type3 + """, """
+                   + name4 + type4 + """, """
+                   + name5 + type5 + """, """
+                   + name6 + type6 + """) """)
     connect.commit()
 
-#Створити базу даних для 7 колонок
-def db_plus7(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
+#Створити адаптовану таблицю для 7 колонок
+def db_plus6(name_table, name1, type1, name2, type2, name3, type3, name4, type4, name5, type5, name6, type6, name7, type7):
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
+                   + name1 + type1 + """, """
+                   + name2 + type2 + """, """
+                   + name3 + type3 + """, """
+                   + name4 + type4 + """, """
+                   + name5 + type5 + """, """
+                   + name6 + type6 + """, """
+                   + name7 + type7 + """) """)
     connect.commit()
 
-#Створити базу даних для 8 колонок
-def db_plus8(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
-    connect.commit()
-
-#Створити базу даних для 9 колонок
-def db_plus9(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
-    connect.commit()
-
-#Створити базу даних для 10 колонок
-def db_plus10(name1, type1, name2, type2, name3, type3):
-    cursor.execute(""" CREATE TABLE IF NOT EXISTS expenses(name TEXT, last_name TEXT, id INT) """)
-    connect.commit()
 
 #Видалити базу даних
 def db_delete():
